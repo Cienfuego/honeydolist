a4879468675dcd6c9c97f95d9b91681b79c16607
mtompkins <mtompkins82@gmail.com>
2025-06-22 11:15:20 -0400

    second commit

---
+++ src/app/pages/hello.ts
@@ -0,0 +1,59 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth';
+
+@Component({
+  selector: 'app-hello',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './hello.html',
+  styleUrls: ['./hello.scss']
+  // template: `
+  //   <div class="hello-container">
+  //     <h2>Hello, World!</h2>
+  //     <p>You are now logged in.</p>
+  //   </div>
+  // `
+
+})
+export class HelloComponent {
+  constructor(private router: Router, private authService: AuthService) {}
+
+  onLogout(): void {
+    //this.authService.logout();
+    this.router.navigate(['/login']);
+  }
+
+
+  username: string | null = null;
+
+  ngOnInit() {
+    this.username = this.authService.getUsername();
+}
+
+onEdit(): void {
+    this.router.navigate(['/edit-profile']);
+  }
+
+onDelete(): void {
+    // You can add a confirmation dialog here before deleting
+    if (confirm('Are you sure you want to delete your account?')) {
+      const username = this.authService.getUsername();
+      console.log(username);
+      if (username) {
+        console.log("made it here");
+        this.authService.deleteUser(username).subscribe({
+          next: () => {
+            //this.authService.logout();
+            //this.router.navigate(['/login']);
+          },
+          error: err => {
+            console.error('Delete failed', err);
+            alert('An error occurred while deleting your account.');
+          }
+        });
+      }
+    }
+  }
+}
